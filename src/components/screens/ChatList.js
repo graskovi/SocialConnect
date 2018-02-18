2770f1e566a0dcfa05664edc8a8f024e601eb402
Glenn Raskovich <graskovich@gmail.com>
2018-02-18 04:19:01 -0800

    Added AtoZList to ChatList, currently implementation is broken

--- src/components/screens/ChatList.js
+++ src/components/screens/ChatList.js
@@ -13,6 +13,7 @@ import { Navigation } from 'react-native-navigation';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import AtoZList from 'react-native-atoz-list';
 import * as reducers from '../../reducers/index';
 /*import registerScreens from './screens';
 
@@ -25,6 +26,36 @@ const logo = require('../../img/SC_Logo.png');
 const blankAvatar = require('../../img/Avatar_Blank.png');
 
 export default class ChatList extends Component {
+  /*let myData = {
+    'A': [{name: 'Aziz'}],
+    'B': [],
+    'C': [],
+    'D': [],
+    'E': [{name: 'Eliezzer'}],
+    'F': [],
+    'G': [],
+    'G': [{name: 'Glenn'}],
+    'H': [{name: 'Henry'}],
+    'I': [],
+    'J': [],
+    'K': [],
+    'L': [],
+    'M': [{name: 'Maria'}],
+    'N': [],
+    'O': [],
+    'P': [],
+    'Q': [],
+    'R': [],
+    'S': [],
+    'T': [{name: 'Tammy'}],
+    'U': [],
+    'V': [],
+    'W': [],
+    'X': [],
+    'Y': [],
+    'z': [],
+  };*/
+
   constructor(props) {
     super(props);
 
@@ -33,6 +64,13 @@ export default class ChatList extends Component {
     this.state = {
       loading: false,
       data: [],
+      myData: [
+        'Aziz',
+        'Glenn',
+        'Henry',
+        'Maria',
+        'Tammy'
+      ],
       page: 1,
       seed: 1,
       error: null,
@@ -109,9 +147,69 @@ export default class ChatList extends Component {
     );
   }
 
+  renderCell(data) {
+    return (
+        <View style={styles.cell}>
+            <View style={[styles.placeholderCircle, { backgroundColor: '#FF0000' }]} />
+            <Text style={styles.name}>
+                {data} {data.split('').reverse().join('')}
+            </Text>
+        </View>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+        <AtoZList
+          sectionHeaderHeight={35}
+          cellHeight={95}
+          data={this.state.myData}
+          renderCell={this.renderCell}
+          renderSection={this.renderHeader}
+        />
+    );
+  }
+} // end of class ChatList
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  alphabetSidebar: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+},
+placeholderCircle: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#ccc',
+    borderRadius: 25,
+    marginRight: 10,
+    marginLeft: 5,
+},
+name: {
+    fontSize: 15,
+},
+cell: {
+    height: 95,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+},
+        /*<View style={styles.container}>
         <FlatList
           data={this.state.data}
           renderItem={({ item }) => (
@@ -127,19 +225,5 @@ export default class ChatList extends Component {
           keyExtractor={item => item.email}
           ListHeaderComponent={this.renderHeader}
         />
-      </View>
-    );
-  }
-} // end of class ChatList
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around'
-  }
+        </View>*/
 });
